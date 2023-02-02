43732d0f28d1557c36acae4167770f761a5745ea
BogdanSaf <115074233+BogdanSaf@users.noreply.github.com>
2023-02-02 17:21:04 +0000

    added basic spring boot template

---
+++ AceMobiles/src/main/java/TeamProject/Group24/AceMobiles/AceMobilesApplication.java
@@ -0,0 +1,13 @@
+package TeamProject.Group24.AceMobiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AceMobilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AceMobilesApplication.class, args);
+	}
+
+}
