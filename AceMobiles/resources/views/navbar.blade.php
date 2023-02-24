0ffeec1fb91f887d148ad451d3f9a5c6a5751e03
samirul2 <99757269+samirul2@users.noreply.github.com>
2023-02-24 19:18:46 +0000

    contact page

--- AceMobiles/resources/views/navbar.blade.php
+++ AceMobiles/resources/views/navbar.blade.php
@@ -5,9 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="{{ asset('css/navbar.css') }}">
+    
 </head>
 <body> -->
+<link rel="stylesheet" href="{{ asset('css/navbar.css') }}">
     <header>
         <div class="logo">AceMobiles</div>
         <div class="hamburger">
