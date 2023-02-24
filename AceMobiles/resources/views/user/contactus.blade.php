97c8f3d974b9622cc7e3f9430151038ba86234fa
samirul2 <99757269+samirul2@users.noreply.github.com>
2023-02-24 14:53:36 +0000

    contact page with nav bar

--- AceMobiles/resources/views/user/contactus.blade.php
+++ AceMobiles/resources/views/user/contactus.blade.php
@@ -7,12 +7,14 @@
   <script src="https://kit.fontawesome.com/c32adfdcda.js" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="{{ asset('css/contactus.css') }}">
 </head>
+
 <body>
-  
+@include('navbar')
   <section>
     
     <div class="section-header">
       <div class="container">
+        
         <h2>Contact Us</h2>
         <p>At Ace Mobiles, we prioritise customer satisfaction. Simply contact us through one of our contact options below if you are unhappy with our service or would like to get in touch and our customer service team will get back to you. </p>
         <br>
