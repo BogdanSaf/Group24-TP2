5c1966f3cf46e23bf6d81e801c7390b6d3d03bb1
Hu3na <115074261+Hu3na@users.noreply.github.com>
2023-02-27 13:58:56 +0000

    changes to syntax
    include nav bar and header

--- AceMobiles/resources/views/user/home.blade.php
+++ AceMobiles/resources/views/user/home.blade.php
@@ -1,16 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>HTML 5 Boilerplate</title>
+    <title>HomePage</title>
+    @include('shared.header')
     <link rel="stylesheet" href="{{ asset('css/homepage.css') }}">
   </head>
   <body>
 	<script src="index.js"></script>
   
-  @include('navbar')
+  @include('shared.navbar')
 
 <!--start of home page-->
 <section class="home" id="home">
