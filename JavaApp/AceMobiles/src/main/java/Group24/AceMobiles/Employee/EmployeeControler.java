f9c146baa2626772efddd94902a369ebf600679b
BogdanSaf <210117691@aston.ac.uk>
2023-03-12 08:23:32 +0000

    Added pages for specific employee and user

--- JavaApp/AceMobiles/src/main/java/Group24/AceMobiles/Employee/EmployeeControler.java
+++ JavaApp/AceMobiles/src/main/java/Group24/AceMobiles/Employee/EmployeeControler.java
@@ -3,7 +3,10 @@ package Group24.AceMobiles.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.math.BigInteger;
 
 @Controller
 public class EmployeeControler {
@@ -15,6 +18,13 @@ public class EmployeeControler {
     public ModelAndView getAllUsers() {
         ModelAndView mav = new ModelAndView("employees");
         mav.addObject("employees", employeeRepository.findAll());
+        return mav;
+    }
+
+    @GetMapping({"/employees/{id}",})
+    public ModelAndView getEmployeeById(@PathVariable BigInteger id){
+        ModelAndView mav = new ModelAndView("specific_employee");
+        mav.addObject("specific_employee", employeeRepository.findById(id).get());
         return mav;
     }
 }
--- JavaApp/AceMobiles/src/main/java/Group24/AceMobiles/Users/UserController.java
+++ JavaApp/AceMobiles/src/main/java/Group24/AceMobiles/Users/UserController.java
@@ -3,7 +3,10 @@ package Group24.AceMobiles.Users;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.math.BigInteger;
 
 @Controller
 public class UserController {
@@ -15,6 +18,13 @@ public class UserController {
     public ModelAndView getAllUsers() {
         ModelAndView mav = new ModelAndView("users");
         mav.addObject("users", userRepository.findAll());
+        return mav;
+    }
+
+    @GetMapping({"/{id}",})
+    public ModelAndView getUserById(@PathVariable BigInteger id) {
+        ModelAndView mav = new ModelAndView("specific_user");
+        mav.addObject("single_user", userRepository.findById(id).get());
         return mav;
     }
 }
