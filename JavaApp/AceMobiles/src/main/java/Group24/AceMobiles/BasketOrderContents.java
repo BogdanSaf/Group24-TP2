ba977a5d4abe6a09126317858277d759b901f033
BogdanSaf <115074233+BogdanSaf@users.noreply.github.com>
2023-03-17 16:13:47 +0000

    BasketOrderContents repository and class. Not working atm

---
+++ JavaApp/AceMobiles/src/main/java/Group24/AceMobiles/BasketOrderContents.java
@@ -0,0 +1,63 @@
+package Group24.AceMobiles;
+
+import Group24.AceMobiles.Orders.Orders;
+import Group24.AceMobiles.Product.Product;
+import jakarta.persistence.*;
+
+import java.math.BigInteger;
+
+@Entity
+@Table(name = "basket_order_contents")
+public class BasketOrderContents {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "basketcontentid")
+    private BigInteger basketOrderContentsId;
+
+    @ManyToOne(targetEntity = Orders.class,fetch = FetchType.LAZY)
+    @JoinColumn(name = "orderidfk", referencedColumnName = "orderid")
+    private Orders orderIdfk;
+
+    @ManyToOne(targetEntity = Product.class,fetch = FetchType.LAZY)
+    @JoinColumn(name = "productidfk", referencedColumnName = "productid")
+    private Product productIdfk;
+
+    @Column(name = "quantity")
+    private int quantity;
+
+    public BasketOrderContents() {
+    }
+
+    public BigInteger getBasketOrderContentsId() {
+        return basketOrderContentsId;
+    }
+
+    public void setBasketOrderContentsId(BigInteger basketOrderContentsId) {
+        this.basketOrderContentsId = basketOrderContentsId;
+    }
+
+    public Orders getOrderIdfk() {
+        return orderIdfk;
+    }
+
+    public void setOrderIdfk(Orders orderIdfk) {
+        this.orderIdfk = orderIdfk;
+    }
+
+    public Product getProductIdfk() {
+        return productIdfk;
+    }
+
+    public void setProductIdfk(Product productIdfk) {
+        this.productIdfk = productIdfk;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
