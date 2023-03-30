46c90a0c8838297bbd8b2b718baf9d3eed44793a
samirul2 <99757269+samirul2@users.noreply.github.com>
2023-03-30 23:31:06 +0100

    Basket page improved front end (Table format)

--- AceMobiles/resources/views/user/basket.blade.php
+++ AceMobiles/resources/views/user/basket.blade.php
@@ -1,118 +1,67 @@
 <!DOCTYPE html>
-
 <html lang="en">
-    
 <head>
   <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Ace Mobiles | Basket</title> 
   <script src="https://kit.fontawesome.com/c32adfdcda.js" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="{{ asset('/css/BasketPage.css') }}">
-
 </head>
 
-<?php $totalPrice = 0;
+<?php 
+  $totalPrice = 0;
   $decoded = json_decode(json_encode($basket), true);
 ?>
 
 <body>
-
-@include('shared.header')
+  @include('shared.header')
   @include('shared.navbar')
 
-<!--basket view-->
+  <div class="wrapper">
+    <h1>Basket</h1> 
 
- <div class = "wrapper">
- <h1>Basket</h1> 
- <div class= "project">
+    <table>
+      <thead>
+        <tr>
+          <th>Product Name</th>
+          <th>Price</th>
+          <th>Quantity</th>
+          <th>Total Price</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        @foreach($decoded as $item)
+          <?php 
+            $totalPrice += $item['totalPrice'] * $item['quantity']; 
+            $imagePath = "/images/" .  $item['productImage']; 
+          ?>
+          <tr>
+            <td>
+              <img src="{{ URL::asset($imagePath)}}" alt="{{ $item['productName'] }}" width="50">
+              {{ $item['productName'] }}
+            </td>
+            <td>£{{ $item['productPrice'] }}</td>
+            <td>
+              <input type="number" value="{{ $item['quantity'] }}" min="1">
+            </td>
+            <td>£{{ $item['totalPrice'] }}</td>
+            <td>
+              <button class="remove-btn">Remove</button>
+            </td>
+          </tr>
+        @endforeach
+      </tbody>
+    </table>
 
+    <div class="summary">
+      <p>
+        Subtotal: <span>£{{ $totalPrice }}</span>
+      </p>
+      <button class="checkout-btn">Checkout</button>
     </div>
-</div>
+  </div>
 
-    <div class= "shop">
-
-            @foreach($decoded as $item)
-            <?php $totalPrice += $item['totalPrice'] * $item['quantity']; $imagePath = "/images/" .  $item['productImage']; ?>
-            <div class = "box">
-            <img src ="{{ URL::asset($imagePath)}}" alt="">
-                 <div class="content">
-             <h3>{{$item['productName']}} </h3>
-             <h4>{{$item['productPrice']}}</h4>
-             <p class="unit">Quantity:<input value="{{$item['quantity']}}"></p>
-             <p class="btn-area">
-                <i class = "fa fa-trash"></i>
-                <span class ="btn2">Remove</span>
-            </p>
-
-        </div>
-        
-    </div>
-    @endforeach
-
-<div class="row">
-        <div class="col-sm-4 alert alert-primary mx-3">
-            <span>
-                <strong>Total: £{{ $totalPrice }}</strong>
-</span>
-</div>
-<div class="col-sm-4">
-    <a href="{{ asset('/checkout') }}" class="btn btn-primary">Checkout</a>
-</div>
-</div>
-</div>
-
-<div style="height: 40px;" class="bg-white">
-
-</div>
-
-
-        
-        <i class="fa fa-trash"></i>
-                <span class ="btn2">Remove</span>
-                    </p>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-
-
-
-<!-- <--!<div class = "box">
-            <img src ="" alt="">
-                 <div class="content">
-             <h3>iPhone 14 Pro Max </h3>
-             <h4> Price: £££</h4>
-             <p class="unit">Quantity: <input value=""></p>
-             <p class="btn-area">
-                <i class = "fa fa-trash"></i>
-                <span class ="btn2">Remove</span>
-            </p>
-
-        </div>
-    </div>
-
-
-
-
-<div class="right-bar">
-        <p><span>Subtotal</span> <span>£££</span></p>
-        <hr>
-        <p><span>Shipping</span> <span>££</span></p>
-        <hr>
-        <p><span>Total</span> <span>£££</span></p>
-        
-        <a href="#"><i class="fa fa-basket"></i>Checkout</a>
-
-        </div>
-    </div>
-</div> -->
-
-<!--insert footer below-->
-
-@include('shared.footer')
-
+  @include('shared.footer')
 </body>
-
 </html>
-
