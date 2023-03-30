68993db3c2ff475dbb554c71a5c7d588a277adcf
James-Hancocks <116155668+James-Hancocks@users.noreply.github.com>
2023-03-30 22:34:45 +0100

    Update AboutUs.blade.php

--- AceMobiles/resources/views/user/AboutUs.blade.php
+++ AceMobiles/resources/views/user/AboutUs.blade.php
@@ -22,7 +22,7 @@
 		
         <div class="home_img">
             <div class="home_img__text_section_container">
-                <img src="phonepeople.jpg" class="home_img__image">
+                <img src={{asset('phonepeople.jpg" class="home_img__image')}}>
                 <div class="home_img__text_section_container">
                     <span class="home_img__text_1">Quality deals for everyone!</span>
                     <span class="home_img__text_2">Our policies ensure that our customers get the best, personalized PRIME deals!</span>
@@ -35,13 +35,13 @@
 			<h3>Thanks to your support, we have won many awards!</h3>
 			<div class="row">
 			  <div class="column">
-				<img src="retailer.png" alt="test1" style="width: 50%">
+				<img src={{asset('retailer.png')}} alt="test1" style="width: 50%">
 			  </div>
 			  <div class="column">
-				<img src="MIA.png" alt="test2" style="width: 50%">
+				<img src="{{asset('MIA.png"')}} alt="test2" style="width: 50%">
 			  </div>
 			  <div class="column">
-				<img src="choice.png" alt="test3" style="width: 50%">
+				<img src="{{asset('choice.png')}} alt="test3" style="width: 50%">
 			  </div>	
 			</div>
     </center>
