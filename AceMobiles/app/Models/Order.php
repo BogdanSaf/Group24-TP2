f77e62e8554e7a6354be3af8e15f5db028c8eb4e
James-Hancocks <116155668+James-Hancocks@users.noreply.github.com>
2023-03-30 22:05:20 +0100

    Update Order.php

--- AceMobiles/app/Models/Order.php
+++ AceMobiles/app/Models/Order.php
@@ -29,4 +29,10 @@ class Order extends Authenticatable
         'arrivalDate',
         'status',
     ];
+    
+     public function orderitems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+    
 }
